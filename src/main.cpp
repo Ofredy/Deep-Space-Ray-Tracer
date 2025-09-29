59d5f6d9b5319bbfa47a00f7c2e60c1a44389495
Ofredy <orellanafredy96@gmail.com>
2025-09-29 12:54:13 -0500

    got to 6.8 rtweekend

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,15 @@
-#include <iostream>
+#include "../inc/rtweekend.h"
 
-#include "../inc/color.h"
-#include "../inc/ray.h"
-#include "../inc/vec3.h"
+#include "../inc/hittable.h"
+#include "../inc/hittable_list.h"
+#include "../inc/sphere.h"
 
-double hit_sphere(const point3& center, double radius, const ray& r) 
+
+color ray_color(const ray& r, const hittable& world) 
 {
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-
-    if (discriminant < 0) 
-    {
-        return -1.0;
-    } else {
-        return (-b - std::sqrt(discriminant) ) / (2.0*a);
-    }
-}
-
-color ray_color(const ray& r) 
-{
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-    if (t > 0.0) 
-    {
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -45,6 +28,13 @@ int main()
     // Calculate the image height, and ensure that it's at least 1.
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    // World
+
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // Camera
 
@@ -68,18 +58,16 @@ int main()
 
     // Render
 
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) 
-    {
+    for (int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) 
-        {
+        for (int i = 0; i < image_width; i++) {
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
